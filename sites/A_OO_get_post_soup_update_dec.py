36bd932f98ce29bb5a038996990ea586ff38ecff
Marius <danmarius7@gmail.com>
2023-11-17 19:55:24 +0200

    Modified get_post  file

--- sites/A_OO_get_post_soup_update_dec.py
+++ sites/A_OO_get_post_soup_update_dec.py
@@ -1,11 +1,20 @@
+#
+#
+#
+# This Decorator will avoid duplicate for me!
+# Respect DRY
+# This decorator have: default headers and soup and update data on peviitor.ro!
+#
 import requests
-import os
+#
+import os  # I do not have API KEY
+#
 import json
 import time
 
 
 DEFAULT_HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.4044.113 Safari/5370.36 Brave/9085',
     'Accept-Language': 'en-US,en;q=0.5',
     'Refer': 'https://google.com',
     'DNT': '1'
@@ -39,6 +48,14 @@ def update_peviitor_api(original_function):
             }
 
         post_request_to_server = requests.post('https://api.peviitor.ro/v4/update/', headers=post_header, data=json.dumps(data_list))
+
+        # don't delete this lines if you want to see the graph on scraper's page
+        file = company_name.lower() + '_scraper.py'
+        data = {'data': len(data_list)}
+        dataset_url = f'https://dev.laurentiumarian.ro/dataset/Scrapers_Dan_Marius/{file}/'
+        requests.post(dataset_url, json=data)
+        ########################################################
+        
         print(json.dumps(data_list, indent=4))
 
         return original_function(*args, **kwargs)

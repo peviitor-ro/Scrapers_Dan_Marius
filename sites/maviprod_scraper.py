e8058d8efcf50ecb1577e1cdca92b61a29dd086e
Marius <danmarius7@gmail.com>
2024-02-01 21:38:22 +0200

    Modified cities fro Maviprod

--- sites/maviprod_scraper.py
+++ sites/maviprod_scraper.py
@@ -5,10 +5,10 @@
 # Acronis job page -> https://www.maviprod.ro/despre-noi/cariere/
 from A_OO_get_post_soup_update_dec import DEFAULT_HEADERS, update_peviitor_api
 from L_00_logo import update_logo
-#
+import unicodedata
 import requests
 from bs4 import BeautifulSoup
-import uuid
+from __utils import get_county
 #
 def collect_data_from_API():
 # function to return a list with JSON data
@@ -19,15 +19,20 @@ def collect_data_from_API():
     for dt in soup_data:
         title=dt.find('a').text
         link=dt.find('a')['href']
-        location = dt.find('p').text.split(': ')[-1]
-        #
+        city = dt.find('p').text.split(': ')[-1]
+        city = unicodedata.normalize('NFKD', city).encode('ascii', 'ignore').decode('utf-8')
+        all_towns = city.split(", ")
+        all_counties=[]
+        for town in all_towns:
+            county=get_county(town)
+            all_counties.append(county)
         list_with_data.append({
-                    "id": str(uuid.uuid4()),
                     "job_title": title,
                     "job_link": link,
                     "company": "maviprod",
                     "country": "Romania",
-                    "city": location
+                    "county": all_counties,                    
+                    "city": all_towns,
                 })
     return list_with_data
 

b26e64e698efb3c055324d6fcf954845d0f84c42
Marius <danmarius7@gmail.com>
2024-02-25 16:00:47 +0200

    Added tests

--- sites/gomag_scraper.py
+++ sites/gomag_scraper.py
@@ -15,29 +15,30 @@ def scraper():
     #print (soup)
     job_list = []
     for job in soup.find_all('div', class_="thrv_wrapper thrv_text_element"):
-        print (job)
-        # job_title=job.find('h2').text.strip()
-        # job_title= unicodedata.normalize('NFKD', job_title).encode('ascii', 'ignore').decode('utf-8')
-        # div_id = re.search(r"<div id=\"([^\"]*)\">", str(job))
-        # the_id = div_id.group(1)
-        # link = "https://altex.ro/cariere/#"+the_id
-        # locations=re.search(r"<!-- --> <!-- -->\s*(.*?)\s*<\/div>", str(job))
-        # towns=locations.group(1).strip()
-        # town_list = list(filter(None, map(lambda town: town.strip(), towns.split(","))))
-        # county_list=[]
-        # for town in town_list:
-        #     county=get_county(town)
-        #     if county is not None:  # Check for existing town
-        #         county_list.append(county)
-        # job_list.append(Item(
-        #     job_title = job_title.title(),
-        #     job_link=link,
-        #     company='Altex',
-        #     country='Romania',
-        #     county=county_list,
-        #     city=town_list,
-        #     remote='on-site',
-        # ).to_dict())
+        if job.select('a'):
+            print (job)
+            # job_title=job.find('h2').text.strip()
+            # job_title= unicodedata.normalize('NFKD', job_title).encode('ascii', 'ignore').decode('utf-8')
+            # div_id = re.search(r"<div id=\"([^\"]*)\">", str(job))
+            # the_id = div_id.group(1)
+            # link = "https://altex.ro/cariere/#"+the_id
+            # locations=re.search(r"<!-- --> <!-- -->\s*(.*?)\s*<\/div>", str(job))
+            # towns=locations.group(1).strip()
+            # town_list = list(filter(None, map(lambda town: town.strip(), towns.split(","))))
+            # county_list=[]
+            # for town in town_list:
+            #     county=get_county(town)
+            #     if county is not None:  # Check for existing town
+            #         county_list.append(county)
+            # job_list.append(Item(
+            #     job_title = job_title.title(),
+            #     job_link=link,
+            #     company='Altex',
+            #     country='Romania',
+            #     county=county_list,
+            #     city=town_list,
+            #     remote='on-site',
+            # ).to_dict())
     return job_list
 
 

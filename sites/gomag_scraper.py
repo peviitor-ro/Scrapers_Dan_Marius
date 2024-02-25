b4fc5eb787af71a100ae567847764d7a0fc53485
Marius <danmarius7@gmail.com>
2024-02-25 21:32:01 +0200

    Added Gomag

--- sites/gomag_scraper.py
+++ sites/gomag_scraper.py
@@ -1,44 +1,47 @@
 import unicodedata
-import re
 from __utils.dynamic_requests_html_shorts import GetDynamicSoup
 from __utils.found_county import get_county
-from __utils.get_job_type import get_job_type
 from __utils.items_struct import Item
 from __utils.peviitor_update import UpdateAPI
 
 
 def scraper():
     '''
-    ... scrape data from gomag scraper.
+    ... scrape data from Gomag scraper.
     '''
     soup = GetDynamicSoup("https://www.gomag.ro/blog/cariere")
     #print (soup)
     job_list = []
     for job in soup.find_all('div', class_="thrv_wrapper thrv_text_element"):
         if job.select('a'):
-            print (job)
-            # job_title=job.find('h2').text.strip()
+            job_title=job.find('a').text.strip()
             # job_title= unicodedata.normalize('NFKD', job_title).encode('ascii', 'ignore').decode('utf-8')
-            # div_id = re.search(r"<div id=\"([^\"]*)\">", str(job))
-            # the_id = div_id.group(1)
-            # link = "https://altex.ro/cariere/#"+the_id
-            # locations=re.search(r"<!-- --> <!-- -->\s*(.*?)\s*<\/div>", str(job))
-            # towns=locations.group(1).strip()
-            # town_list = list(filter(None, map(lambda town: town.strip(), towns.split(","))))
-            # county_list=[]
-            # for town in town_list:
-            #     county=get_county(town)
-            #     if county is not None:  # Check for existing town
-            #         county_list.append(county)
-            # job_list.append(Item(
-            #     job_title = job_title.title(),
-            #     job_link=link,
-            #     company='Altex',
-            #     country='Romania',
-            #     county=county_list,
-            #     city=town_list,
-            #     remote='on-site',
-            # ).to_dict())
+            link = job.find('a')['href']
+            locations=[]
+            if 'Baia Mare' not in job_title and 'Bucuresti' not in job_title:
+                locations.append('Baia Mare')
+            if 'Baia Mare' in job_title:
+                locations.append('Baia Mare')
+            if 'Bucuresti' in job_title:
+                locations.append('Bucuresti')
+            county_list=[]
+            for town in locations:
+                county=get_county(town)
+                if county is not None:  # Check for existing town
+                    county_list.append(county)
+            if 'remote' in job_title:
+                spot='remote'
+            else:
+                spot='on-site'
+            job_list.append(Item(
+                job_title = job_title,
+                job_link=link,
+                company='Gomag',
+                country='Romania',
+                county=county_list,
+                city=locations,
+                remote=spot,
+            ).to_dict())
     return job_list
 
 
@@ -49,14 +52,14 @@ def main():
     ---> update_jobs() and update_logo()
     '''
 
-    company_name = "gomag"
-    logo_link = "logo_link"
+    company_name = "Gomag"
+    logo_link = "https://theme.zdassets.com/theme_assets/2161170/a765589a7ac93d2e84133179756f153cd77f4f5e.png"
 
     jobs = scraper()
 
-    # uncomment if your scraper done
-    #UpdateAPI().update_jobs(company_name, jobs)
-    #UpdateAPI().update_logo(company_name, logo_link)
+
+    UpdateAPI().update_jobs(company_name, jobs)
+    UpdateAPI().update_logo(company_name, logo_link)
 
 
 if __name__ == '__main__':
